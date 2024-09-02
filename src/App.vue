716667f9971a47c5fe7afbabe5ea30dbd801331a
muratkoyun <mkoyun@red.software.systems>
2024-09-02 17:48:04 +0200

    component props communication

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,20 @@
 <template>
-  <div>
-    <h1>{{ title }}</h1>
-    <cats-list></cats-list>
-  </div>
+  <section>
+    <header>
+      <h1>{{ title }}</h1>
+    </header>
+    <ul>
+      <cats-list
+        v-for="cat in cats"
+        :key="cat.id"
+        :catName="cat.name"
+        :catAge="cat.age"
+        :catBreed="cat.breed"
+        :is-favorite="1"
+      >
+      </cats-list>
+    </ul>
+  </section>
 </template>
 
 <script>
@@ -10,9 +22,79 @@ export default {
   data() {
     return {
       title: "Cats",
+      cats: [
+        {
+          id: 1,
+          name: "Mirko",
+          age: 3,
+          breed: "European",
+        },
+        {
+          id: 2,
+          name: "Sylvester",
+          age: 8,
+          breed: "Persian",
+        },
+      ],
     };
   },
 };
 </script>
 
-<style></style>
+<style>
+* {
+  box-sizing: border-box;
+}
+html {
+  font-family: "Jost", sans-serif;
+}
+body {
+  margin: 0;
+}
+header {
+  box-shadow: 0 2px 8px rgba(0, 0, 0, 0.26);
+  margin: 3rem auto;
+  border-radius: 10px;
+  padding: 1rem;
+  background-color: #58004d;
+  color: white;
+  text-align: center;
+  width: 90%;
+  max-width: 40rem;
+}
+#app ul {
+  margin: 0;
+  padding: 0;
+  list-style: none;
+}
+#app li {
+  box-shadow: 0 2px 8px rgba(0, 0, 0, 0.26);
+  margin: 1rem auto;
+  border-radius: 10px;
+  padding: 1rem;
+  text-align: center;
+  width: 90%;
+  max-width: 40rem;
+}
+#app h2 {
+  font-size: 2rem;
+  border-bottom: 4px solid #ccc;
+  color: #58004d;
+  margin: 0 0 1rem 0;
+}
+#app button {
+  font: inherit;
+  cursor: pointer;
+  border: 1px solid #ff0077;
+  background-color: #ff0077;
+  color: white;
+  padding: 0.05rem 1rem;
+  box-shadow: 1px 1px 2px rgba(0, 0, 0, 0.26);
+}
+#app button:hover,
+#app button:active {
+  background-color: #ec3169;
+  border-color: #ec3169;
+  box-shadow: 1px 1px 4px rgba(0, 0, 0, 0.26);
+}
+</style>
--- src/components/CatsList.vue
+++ src/components/CatsList.vue
@@ -1,39 +1,68 @@
 <template>
-  <div>
-    <button @click="toggleVisibility()">
-      {{ isVisible ? "hide" : "show" }} cats
+  <li>
+    <h2>{{ catName }} {{ isCatFavorite === "1" ? "(Fav)" : "" }}</h2>
+    <button @click="toggleFav">toggle Fav</button>
+    <button @click="toggleVisibility">
+      {{ isVisible ? "Hide" : "Show" }} Details
     </button>
     <ul v-if="isVisible">
-      <li :key="cat.age" v-for="cat in cats">
-        {{ cat.name }} - {{ cat.age }} years old
+      <li>
+        <strong>Age:</strong>
+        {{ catAge }}
+      </li>
+      <li>
+        <strong>Breed:</strong>
+        {{ catBreed }}
       </li>
     </ul>
-  </div>
+  </li>
 </template>
 
 <script>
 export default {
+  // props: ["catName", "catAge", "catBreed", "isFavorite"],
+  props: {
+    catName: {
+      type: String,
+      required: true,
+    },
+    catAge: {
+      type: String,
+      required: true,
+    },
+    catBreed: String,
+    isFavorite: {
+      type: String,
+      default: "0",
+      required: false,
+      // validator: function (val) {
+      //   return val === "1" || val === "0";
+      // },
+    },
+  },
   name: "CatsList",
   data() {
     return {
+      isCatFavorite: this.isFavorite,
       isVisible: false,
-      cats: [
-        {
-          name: "Mirko",
-          age: 3,
-          breed: "European",
-        },
-        {
-          name: "Sylvester",
-          age: 8,
-          breed: "Persian",
-        },
-      ],
+      cats: {
+        name: "Mirko",
+        age: 3,
+        breed: "European",
+      },
     };
   },
   methods: {
     toggleVisibility() {
       this.isVisible = !this.isVisible;
+    },
+    toggleFav() {
+      /// props can not be mutated directly!!!  error  Unexpected mutation of "isFavorite" prop
+      if (this.isCatFavorite === "1") {
+        this.isCatFavorite = "0";
+      } else {
+        this.isCatFavorite = "1";
+      }
     },
   },
 };
